a60dbf1159397e2ea62be6b94113075501296003
ThanhcongMssl <thanhcong.mssl@gmail.com>
2015-03-21 22:18:14 +0700

    fix require config

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,9 @@
 'use strict';
 
-require.config({
-    shim: {
-        angular: {
-            exports: 'angular'
-        },
-        chartjs: {
-        	exports: 'Chart'
-        },
-        owlCarousel : {
-            exports: 'owlCarousel',
-            deps: ['jquery']
-        }
-    },
-    paths: {
-        text: '../bower_components/requirejs-text/text',
-        angular: '../bower_components/angular/angular',
-        chartjs: '../bower_components/chartjs/chart',
-        jquery: '../bower_components/jquery/dist/jquery.min',
-        owlCarousel: '../bower_components/owl-carousel/owl-carousel/owl.carousel'
-    }
-});
+/* global require */
 
-require(['angular', 'controller/index', 'directive/index'], function(angular) {
- 	angular.bootstrap(document, ['portfolio']);
+require(['config'], function(){
+    require(['angular', 'controller/index', 'directive/index'], function(angular) {
+        angular.bootstrap(document, ['portfolio']);
+    });
 });
