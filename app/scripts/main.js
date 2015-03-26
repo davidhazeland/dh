463d630af8e0e4d5d9e0044ba4c8b164ec13f5e8
ThanhcongMssl <thanhcong.mssl@gmail.com>
2015-03-26 10:18:38 +0700

    add loading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,10 @@
 
 /* global require */
 
-require(['config'], function(){
-   require(['angularjs', 'filter/index', 'controller/index', 'directive/index'], function(angular) {
-        angular.bootstrap(document, ['portfolio']);
-    });  
+require(['config'], function() {
+	document.body.classList.add('loading'); 
+	require(['angularjs', 'filter/index', 'controller/index', 'directive/index'], function(angular, $) {
+		document.body.classList.remove('loading');
+		angular.bootstrap(document, ['portfolio']);
+	});
 });
